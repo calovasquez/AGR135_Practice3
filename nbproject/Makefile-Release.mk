c79829e48e8fcef2e0c9c4fe9ce174953ebd8216
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 21:41:36 -0600

    Added keyboard_pointer.c

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/keyboard_pointer.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/agr135_practice3: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/agr135_practice3 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/keyboard_pointer.o: keyboard_pointer.c
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/keyboard_pointer.o keyboard_pointer.c
 
 ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
