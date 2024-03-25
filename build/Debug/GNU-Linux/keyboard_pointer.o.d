c0aeed4f8af92549865fb17e5d5e25afe098bc87
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 21:57:48 -0600

    Updated project

---
+++ build/Debug/GNU-Linux/keyboard_pointer.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux/keyboard_pointer.o: keyboard_pointer.c \
+ keyboard_pointer.h
+keyboard_pointer.h:
--- build/Debug/GNU-Linux/main.o.d
+++ build/Debug/GNU-Linux/main.o.d
@@ -1,1 +1,2 @@
-build/Debug/GNU-Linux/main.o: main.c
+build/Debug/GNU-Linux/main.o: main.c keyboard_pointer.h
+keyboard_pointer.h:
