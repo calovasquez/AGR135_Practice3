c38d5c0ddb3259d436474d998ba20b12768a5cea
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 22:19:35 -0600

    Changed functions names

--- main.c
+++ main.c
@@ -30,8 +30,8 @@ int main(int argc, char** argv) {
     glClearColor(1, 1, 1, 1); // White color
     initialize();
     glutDisplayFunc(draw);
-    glutKeyboardFunc(keyboard_mt);
-    glutMouseFunc(pointer_mt);
+    glutKeyboardFunc(keyEvent);
+    glutMouseFunc(mouseEvent);
     glutMainLoop();
     return (EXIT_SUCCESS);
 }
