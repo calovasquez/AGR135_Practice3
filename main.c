7516b7450a461ec397dfd70e64fe4e9797f663ae
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 20:24:45 -0600

    Initial project

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: vasquez
+ *
+ * Created on March 24, 2024, 8:18 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
