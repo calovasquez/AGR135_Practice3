ae6451fa352bc3f45168ec113153ebc1f96db873
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 21:50:12 -0600

    Added initialize function

--- keyboard_pointer.c
+++ keyboard_pointer.c
@@ -6,3 +6,9 @@
 #include <GL/freeglut.h>
 #include "./keyboard_pointer.h"
 
+// Set the background color and matrix mode
+void initialize(void){
+    glClearColor(1, 1, 1, 1); // White color
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 700, 0, 700); // 700x700
+}
