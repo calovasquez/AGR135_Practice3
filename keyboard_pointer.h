ebfe72f4627e7422259da4c394772bb30620fde0
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 21:42:23 -0600

    Added keyboard_pointer.h

---
+++ keyboard_pointer.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   keyboard_pointer.h
+ * Author: vasquez
+ *
+ * Created on March 24, 2024, 9:41 PM
+ */
+
+#ifndef KEYBOARD_POINTER_H
+#define KEYBOARD_POINTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* KEYBOARD_POINTER_H */
