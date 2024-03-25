7ad2153f78caad64e197ed4b542f22a7ee15b2d2
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 21:50:58 -0600

    Added main function

--- keyboard_pointer.c
+++ keyboard_pointer.c
@@ -12,3 +12,11 @@ void initialize(void){
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0, 700, 0, 700); // 700x700
 }
+
+// Main function
+void draw(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+        cube();
+    glFlush();
+}
+
