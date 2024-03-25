8e3f6eb82307ebee3522516185d1933d5990867e
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-24 22:33:03 -0600

    Added comment

--- keyboard_pointer.h
+++ keyboard_pointer.h
@@ -20,7 +20,7 @@ extern "C" {
 // Headers
 void initialize(void); // Projection function
 void draw(void); // Main function
-void cube();
+void cube(); // Function where the cube is drawn
 void keyboard_mt(unsigned char,int,int); // Keyboard event function
 void pointer_mt(int,int,int,int); // Pointer event function
 #endif /* KEYBOARD_POINTER_H */
